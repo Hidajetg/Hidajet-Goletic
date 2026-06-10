f3eebf0e6862327227792be832950d856f4ecbea
Hidajet Goletic <hidajetgoletic@gmail.com>
2026-06-10 09:09:16 +0200

    fix cars permissions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 export default function LoginPage() {
-  const [name, setName] = useState("Admin");
+  const [name, setName] = useState("");
   const [pin, setPin] = useState("");
   const [showPin, setShowPin] = useState(false);
 
